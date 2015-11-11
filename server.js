67ebad0c190fc9af296680b68d6a546606d53cf2
Oleg Solomka <legomushroom@gmail.com>
2015-11-11 13:56:50 +0200

    add node server

--- server.js
+++ server.js
@@ -4,21 +4,35 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // serve static assets normally
-app.use(express.static(__dirname + '/front-end/'))
+app.use(express.static(__dirname + '/front-end/'));
+
+var rewriteForPath = function (p, url, response) {
+  var url = path.parse(path);
+  console.log(url);
+  response.sendFile(path.resolve(__dirname, 'front-end', 'index.html'))
+}
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response){
   var url = path.parse(request.url);
-  if (!url.base.match(/\.js/)) {
-    response.sendFile(path.resolve(__dirname, 'front-end', 'index.html'))
-  } else {
-    if (url.base === 'prism.min.js') {
-      response.sendFile(path.resolve(__dirname, 'front-end/app/vendor/', path.parse(request.url).base ))
-    } else {
-      response.sendFile(path.resolve(__dirname, 'front-end/dist/', path.parse(request.url).base ))
-    }
-  }
+
+  rewriteForPath('dist', request.url, response);
+
+  var url = path.parse(path);
+  console.log(url);
+  response.sendFile(path.resolve(__dirname, 'front-end', 'index.html'))
+
+  // if (!(url.ext === '.js') ) {
+  //   response.sendFile(path.resolve(__dirname, 'front-end', 'index.html'))
+  // } else {
+  //   if (url.base === 'prism.min.js') {
+  //     response.sendFile(path.resolve(__dirname, 'front-end/app/vendor/', path.parse(request.url).base ))
+  //   }
+  //   if (url.base === 'main.js') {
+  //     response.sendFile(path.resolve(__dirname, 'front-end/dist/', path.parse(request.url).base ))
+  //   }
+  // }
 })
 
 app.listen(port)
